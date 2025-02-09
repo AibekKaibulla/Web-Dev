f037b083bc5493f918863bbe2f4069c137b1d77f
Aibek <a_kaibulla@kbtu.kz>
2025-02-09 19:29:45 +0500

    lab3

---
+++ Lab3/javascript.info/js/javascript-fundamentals.js
@@ -0,0 +1,27 @@
+// alert("Hello, world");
+// alert("Hello, world");
+
+// alert(3 + 
+//     1 +
+//     2
+// );
+
+alert("Hello");
+
+[1, 2].forEach(alert);
+
+// This comment occupies a line of its own
+alert('Hello');
+
+alert('World'); // This comment follows the statement
+
+/* An example with two messages.
+This is a multiline comment.
+*/
+alert('Hello');
+alert('World');
+
+// /*
+//   /* nested comment ?!? */
+// */
+//  alert( 'World' );
---
+++ Lab3/javascript.info/js/variables.js
@@ -0,0 +1,36 @@
+// // let message = 'Hello!'; // define the variable and assign the value
+
+// // alert(message); // Hello!
+
+// let user = 'John', age = 25, message = 'Hello';
+
+// alert(
+//     "Hello, my name is " + user + "\n" +
+//     "I'm " + age + " years old." + "\n"
+// );
+
+// // Examples of valid names
+// let userName;
+// let test123;
+
+// let $ = 1; // declared a variable with the name "$"
+// let _ = 2; // and now a variable with the name "_"
+
+// alert($ + _); // 3
+
+// // let 1a; // cannot start with a digit
+
+// // let my-name; // hyphens '-' aren't allowed in the name
+
+let name = 'John';
+let admin = name;
+
+let ourPlanetName = 'Earth';
+let currentUserName = 'Aibek';
+
+const birthday = '18.04.1982';
+
+const age = someCode(birthday);
+
+
+alert(admin);
